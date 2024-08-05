8036720784ac87bb0a8027b499faa761ba8a820f
moonjiyoung <moonqazwsx@naver.com>
2024-08-05 21:42:45 +0900

    240805

---
+++ assets/Js/index.js
@@ -0,0 +1,46 @@
+/*========== nav */
+$('nav .nav_wrapper li').on('click', (e) => {
+  $('nav .nav_wrapper li').removeClass('show');
+  $(e.currentTarget).addClass('show')
+ });
+
+
+/*========== category */
+/*===== scroll Event  */
+let scroll = 5;
+$(window).on('scroll', (e) => {
+  let windowScroll = $(e.target).scrollTop();
+  if (windowScroll > scroll) {
+    $('.category').addClass('active');
+  } else {
+    $('.category').removeClass('active');
+  }
+})
+
+
+/*========== competition */
+ let $competitionNavBtn = $('.competition .competitionNav_wrapper li');
+ let currentIdx = 0;
+
+ $competitionNavBtn.on('click', (e) => {
+   const idx = $(e.currentTarget).index();
+
+   $('.youtuber_container').stop().animate({
+     marginLeft: -100 * idx + '%'
+   }, 1000);
+
+   $competitionNavBtn.removeClass('show');
+   $(e.currentTarget).addClass('show');
+   
+   currentIdx = idx;
+ })
+
+ 
+/*========== comparison */
+
+
+/*========== popularVideo */
+/*===== moreBtn */
+$('.popularVideo .more_container .moreBtn_wrapper').on('click', () => {
+  $('.popularVideo .moreBox').show();
+});
