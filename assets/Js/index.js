f38b44d95928bae37e030ce141b79acbe49df30c
moonjiyoung <moonqazwsx@naver.com>
2024-08-06 21:01:33 +0900

    240806

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -18,9 +18,57 @@ $(window).on('scroll', (e) => {
   }
 })
 
+
+/*===== slide drag */
+  const slider = $(".comparisonTarget_wrapper");
+
+  let x, left, down;
+
+  slider.mousedown(function (e) {
+    e.preventDefault();
+    down = true;
+    x = e.pageX;
+    left = $(this).scrollLeft();
+  });
+
+  slider.mousemove(function (e) {
+    if (down) {
+      var newX = e.pageX;
+      $(this).scrollLeft(left - newX + x);
+    }
+  });
+
+  slider.mouseleave(function (e) {
+    down = false;
+  });
+
+  slider.mouseup(function (e) {
+    down = false;
+  });
+
+
 /*===== slide btn */
 
 
+/*===== bookmark toggle btn */
+$(".category .youtuberList_container .youtuberList_wrapper .comparisonTarget_container ul li .icon").on("click", function(e) {
+  $(e.currentTarget).children().attr("src", function(index, attr){
+    if(attr.match("bookmark_line")){
+      return attr.replace("bookmark_line.png", "bookmark.png");
+        } else{
+          return attr.replace("bookmark.png", "bookmark_line.png");
+      }
+    });
+  });
+  $('.category .youtuberList_container .youtuberList_wrapper .comparisonTarget_container ul li .icon').on('click', (e) => {
+    $(e.currentTarget).toggleClass('toggle');
+  });
+
+
+/*===== profile change toggle btn */
+$('.category .youtuberList_container .youtuberList_wrapper .currentProfile_container').on('click', (e) => {
+  $(".profileChange_wrapper").toggleClass('toggle');
+});
 
 
 
